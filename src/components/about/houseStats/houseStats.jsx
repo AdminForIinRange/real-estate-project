b3edd68e4a4fd32fbfdec913cfe62a1503284e73
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-26 20:47:17 +0930

    commit

--- src/components/about/houseStats/houseStats.jsx
+++ src/components/about/houseStats/houseStats.jsx
@@ -17,67 +17,72 @@ import {
   useMediaQuery,
   Divider,
 } from "@chakra-ui/react";
-import  { PureComponent } from "react";
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { PureComponent } from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 import { FaArrowUp } from "react-icons/fa6";
 
-
-const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
- 
-  
+import chart from "@/Svg/chart.png";
 
 const HouseStats = () => {
-    
   const stats = [
     { label: "Weekly", percentage: "1.9%" },
     { label: "Monthly", percentage: "7.6%" },
     { label: "Yearly", percentage: "37.2%" },
   ];
 
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
   return (
     <>
       <Box
@@ -116,7 +121,6 @@ const HouseStats = () => {
             h={"28px"}
             bg={"green.200"}
             rounded={"10px"}
-          
             zIndex={"1"}
           >
             <HStack
@@ -139,16 +143,25 @@ const HouseStats = () => {
           </Box>
         </HStack>
 
-<Divider mt={"5px"} borderColor={"lack"} />
-        <HStack 
+        <Divider mt={"5px"} borderColor={"lack"} />
+        <Box
+          mt={"10px"}
+          w={"100%"}
+          h={"100px"}
+          boxShadow={"0px 0px 20px rgba(0,0,0,0.25)"}
+          rounded={"10px"}
+          bgImage={`${chart}`}
+          bgSize={"cover"}
+          bgPos={["center", " center", "center", "center", "center"]}
+          bgRepeat={"no-repeat"}
+        ></Box>
+        <HStack
           justify={"center"}
           align={"start"}
           w={"100%"}
           h={"100%"}
           gap={"25px"}
           mt={"10px"}
-      
-        
         >
           {" "}
           {stats.map(({ label, percentage }, index) => (
@@ -168,7 +181,6 @@ const HouseStats = () => {
                   h={"28px"}
                   bg={"green.200"}
                   rounded={"10px"}
-                
                   zIndex={"1"}
                 >
                   <HStack
@@ -192,29 +204,7 @@ const HouseStats = () => {
               </VStack>
             </>
           ))}
-          
-
-
-        
-
-
         </HStack>
-                    
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={500}
-            height={400}
-            data={data}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-          </AreaChart>
-        </ResponsiveContainer>
-    
       </Box>
     </>
   );
