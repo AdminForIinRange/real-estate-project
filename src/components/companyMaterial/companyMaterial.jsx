112e0d05d3680925ae21dd41bc98210a3f5f72b5
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-28 20:59:36 +0930

    Company Material

--- src/components/companyMaterial/companyMaterial.jsx
+++ src/components/companyMaterial/companyMaterial.jsx
@@ -22,7 +22,19 @@ import {
 import { FaFilePdf } from "react-icons/fa";
 import inwardCross from "@/Svg/inwardCross.svg";
 import Image from "next/image";
+
 const CompanyMaterial = () => {
+  const PdfDownload = [
+    {
+      id: 1,
+    },
+    {
+      id: 2,
+    },
+    {
+      id: 3,
+    },
+  ];
   return (
     <>
       <HStack
@@ -35,7 +47,7 @@ const CompanyMaterial = () => {
         <HStack px={"50px"} gap={"5px"} textColor={"#393939"}>
           <Image src={inwardCross} width={100} height={100} />
           <Text
-            fontSize={["40px", "40px", "40px", "50px", "70px"]}
+            fontSize={["30px", "40px", "40px", "50px", "70px"]}
             fontFamily={"Raleway"}
             fontWeight={"500"}
           >
@@ -56,114 +68,45 @@ const CompanyMaterial = () => {
           px={["0px", "10px", "30px", "40px", "60px"]}
           wrap={["wrap", "wrap", "wrap", "wrap", "wrap"]}
         >
-
-
-<Box
-            w={["170px", "500px", "500px", "325px", "500px"]}
-            h={["225px", "300px", "290px", "250px", "250px"]}
-            maxW={["200px", "200px", "250px", "100%", "100%"]}
-            shadow={"0px 0px 20px rgba(0,0,0,0.25)"}
-            p={"10px"}
-            rounded={"10px"}
-            
-          >
-            <HStack w={"100%"} h={"100%"} align={"end"} justify={"center"}>
-              <Box
-                _hover={{
-
-                    cursor:"pointer",
-                    bgColor:"#FFC5C5",
-                    transform:"scale(1.05)"
+          {PdfDownload.map(({ id }) => (
+            <Box
+              key={id}
+              w={["170px", "500px", "500px", "325px", "500px"]}
+              h={["225px", "300px", "290px", "250px", "250px"]}
+              maxW={["200px", "200px", "250px", "100%", "100%"]}
+              shadow={"0px 0px 20px rgba(0,0,0,0.25)"}
+              p={"10px"}
+              rounded={"10px"}
+            >
+              <HStack w={"100%"} h={"100%"} align={"end"} justify={"center"}>
+                <Box
+                  _hover={{
+                    cursor: "pointer",
+                    shadow: "0px 0px 20px red",
+                    transform: "scale(1.05)",
                   }}
-              transition={"all 0.3s ease-in-out"}
-                w={"50%"}
-                h={"20%"}
-                rounded={"10px"}
-                shadow={"0px 0px 20px rgba(0,0,0,0.25)"}
-              >
-                <HStack
-                  w={"100%"}
-                  h={"100%"}
-                  align={"center"}
-                  justify={"center"}
+                  transition={"all 0.3s ease-in-out"}
+                  w={["80%", "80%", "60%", "50%", "50%"]}
+                  h={["15%", "15%", "20%", "20%", "20%"]}
+                  rounded={"10px"}
+                  shadow={"0px 0px 20px rgba(0,0,0,0.25)"}
                 >
-                  <FaFilePdf fontSize={"25px"} />
-                  <Text>Download PDF</Text>
-                </HStack>
-              </Box>
-            </HStack>
-          </Box>
-          <Box
-            w={["170px", "500px", "500px", "325px", "500px"]}
-            h={["225px", "300px", "290px", "250px", "250px"]}
-            maxW={["200px", "200px", "250px", "100%", "100%"]}
-            shadow={"0px 0px 20px rgba(0,0,0,0.25)"}
-            p={"10px"}
-            rounded={"10px"}
-            
-          >
-            <HStack w={"100%"} h={"100%"} align={"end"} justify={"center"}>
-              <Box
-              _hover={{
-
-                cursor:"pointer",
-                bgColor:"#FFC5C5",
-                transform:"scale(1.05)"
-              }}
-            
-              transition={"all 0.3s ease-in-out"}
-                w={"50%"}
-                h={"20%"}
-                rounded={"10px"}
-                shadow={"0px 0px 20px rgba(0,0,0,0.25)"}
-              >
-                <HStack
-                  w={"100%"}
-                  h={"100%"}
-                  align={"center"}
-                  justify={"center"}
-                >
-                  <FaFilePdf fontSize={"25px"} />
-                  <Text>Download PDF</Text>
-                </HStack>
-              </Box>
-            </HStack>
-          </Box>
-          <Box
-            w={["170px", "500px", "500px", "325px", "500px"]}
-            h={["225px", "300px", "290px", "250px", "250px"]}
-            maxW={["200px", "200px", "250px", "100%", "100%"]}
-            shadow={"0px 0px 20px rgba(0,0,0,0.25)"}
-            p={"10px"}
-            rounded={"10px"}
-
-          >
-            <HStack w={"100%"} h={"100%"} align={"end"} justify={"center"}>
-              <Box
-               _hover={{
-
-                cursor:"pointer",
-                bgColor:"#FFC5C5",
-                transform:"scale(1.05)"
-              }}
-              transition={"all 0.3s ease-in-out"}
-                w={"50%"}
-                h={"20%"}
-                rounded={"10px"}
-                shadow={"0px 0px 20px rgba(0,0,0,0.25)"}
-              >
-                <HStack
-                  w={"100%"}
-                  h={"100%"}
-                  align={"center"}
-                  justify={"center"}
-                >
-                  <FaFilePdf fontSize={"25px"} />
-                  <Text>Download PDF</Text>
-                </HStack>
-              </Box>
-            </HStack>
-          </Box>
+                  <HStack
+                    w={"100%"}
+                    h={"100%"}
+                    align={"center"}
+                    justify={"center"}
+                  >
+                    <FaFilePdf fontSize={"25px"} />
+                    <Text fontWeight={"600"} fontSize={["13px", "13px", "13px", "18px", "20px"]}>
+                      {" "}
+                      Download{" "}
+                    </Text>
+                  </HStack>
+                </Box>
+              </HStack>
+            </Box>
+          ))}
         </HStack>
       </HStack>
     </>
